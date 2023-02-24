3c6116c0ce9a1220259585b8a08a4cb11fc6b655
shensq <shensq@umich.edu>
2023-02-23 21:52:48 -0500

    Add logging

--- LLM_request.py
+++ LLM_request.py
@@ -1,6 +1,7 @@
 import argparse
-import pickle
 import logging
+import transformers
+import datasets
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer
 
@@ -12,7 +13,21 @@ def main():
     parser.add_argument('--checkpoint', type=str, default="facebook/opt-iml-max-1.3b")
     args = parser.parse_args()
 
+    # Setup logging
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    log_level = logging.INFO
+    logger.setLevel(log_level)
     logging.info(args)
+
+    datasets.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
 
     logging.info("Loading Dataset.")
     folder = "data/country_prediction/"
